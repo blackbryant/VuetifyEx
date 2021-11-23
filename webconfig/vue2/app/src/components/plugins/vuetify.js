fc09e1365ddf6f6653ef248fc44a122161e4a485
blackbryant <e49134030e@gmail.com>
2021-11-23 18:21:39 +0800

    Create vuetify.js

---
+++ webconfig/vue2/app/src/components/plugins/vuetify.js
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify'
+
+Vue.use(Vuetify);
+
+import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css' 
+import '@mdi/font/css/materialdesignicons.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+
+
+export default new Vuetify({
+     
+});
+ 
